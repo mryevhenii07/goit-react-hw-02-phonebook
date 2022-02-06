7a833177e13ffe03a2314ca70b2d6458e25c1496
mryevhenii07 <mrit.works07@gmail.com>
2022-02-06 22:03:58 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
+import React from "react";
 import "./App.css";
+import shortid from "shortid";
 
-function App() {
-  return <div className="App">hellooo</div>;
+// import { nanoid } from "nanoid";
+import ContactList from "./components/ContactList/ContactList";
+// import { ContactForm } from "./components/ContactForm/ContactForm";
+import { Filter } from "./components/Filter/Filter";
+import listJ from "./list.json";
+import s from "./components/Filter/Filte.module.css";
+import Form from "./components/ContactForm/ContactForm";
+import { nanoid } from "nanoid";
+class App extends React.Component {
+  state = {
+    contacts: listJ,
+    filter: "",
+  };
+  listId = nanoid();
+
+  addList = (number) => {
+    console.log(number);
+    const list = {
+      id: this.nameId,
+      number: number.number,
+      name: number.name,
+    };
+    this.setState((prevState) => ({
+      contacts: [list, ...prevState.contacts],
+    }));
+  };
+  onDeleteList = (deleteId) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((item) => item.id !== deleteId),
+    }));
+  };
+
+  changeFilter = (e) => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+  nameId = shortid.generate();
+  render() {
+    const { contacts, filter } = this.state;
+    const totalContacts = contacts.length;
+    const visibleFilter = this.state.filter.toLowerCase();
+    const visibleList = this.state.contacts.filter((lis) =>
+      lis.name.toLowerCase().includes(visibleFilter)
+    );
+    return (
+      <div className="App">
+        <h2>Phonebook</h2>
+        <Form onSubmit={this.addList} />
+        <h2>Contacts</h2>
+        <span className={s.filterSubTitle}>
+          FInd contacts by name: {totalContacts}
+        </span>
+        <Filter value={filter} changeFilter={this.changeFilter} />
+
+        <ContactList contacts={visibleList} onDeleteList={this.onDeleteList} />
+      </div>
+    );
+  }
 }
 
 export default App;
